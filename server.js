787ccf0e0624b463b7ae41a4e14d1c68aca3bb0d
Mahipal Yadav <silenttalesoriginal@gmail.com>
2026-07-28 23:10:03 +0530

    live counter add

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const path = require("path");
 
 const app = express();
-
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/data", express.static(path.join(__dirname, "data")));
 
 const PORT = process.env.PORT || 3000;
+let onlineUsers = 0;
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+
+  onlineUsers++;
+
+  io.emit("onlineUsers", onlineUsers);
+
+  socket.on("disconnect", () => {
+
+    onlineUsers--;
+
+    io.emit("onlineUsers", onlineUsers);
+
+  });
+
+});
+server.listen(PORT, () => {
   console.log("PyqKing running at http://localhost:" + PORT);
 });
